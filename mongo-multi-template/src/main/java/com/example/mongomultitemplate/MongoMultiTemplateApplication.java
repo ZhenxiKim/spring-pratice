5e38bc7d2901dca49828b9925d95710d48a7be5e
jeongheeKim <8055kjh@gmail.com>
2023-04-07 00:28:22 +0900

    init commit

---
+++ mongo-multi-template/src/main/java/com/example/mongomultitemplate/MongoMultiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongomultitemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoMultiTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoMultiTemplateApplication.class, args);
+    }
+
+}
---
+++ mongo-multi-template/src/test/java/com/example/mongomultitemplate/MongoMultiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongomultitemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoMultiTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
